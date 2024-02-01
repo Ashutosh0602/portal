628030304d255de8a70f8dc2482578cdd8514729
Ashutosh Rai <ashu00jnp@gmail.com>
2024-02-01 19:31:47 +0530

    update lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
 import "./App.css";
-import Home from "./pages/home/Home";
 import { Route, Routes, useLocation } from "react-router-dom";
-import Blog from "./pages/blog/Blog";
-import Experience from "./pages/experience/Experience";
-import Project from "./pages/project/Project";
 import { AnimatePresence } from "framer-motion";
-import ProjectDetail from "./pages/projectDetail/ProjectDetail";
-import Contact from "./pages/contact/Contact";
-import About from "./pages/about/About";
+import { lazy } from "react";
+
+// import Home from "./pages/home/Home";
+const Home = lazy(() => import("./pages/home/Home"));
+
+// import Blog from "./pages/blog/Blog";
+const Blog = lazy(() => import("./pages/blog/Blog"));
+
+// import Experience from "./pages/experience/Experience";
+const Experience = lazy(() => import("./pages/experience/Experience"));
+
+// import Project from "./pages/project/Project";
+const Project = lazy(() => import("./pages/project/Project"));
+
+// import ProjectDetail from "./pages/projectDetail/ProjectDetail";
+const ProjectDetail = lazy(() => import("./pages/projectDetail/ProjectDetail"));
+
+// import Contact from "./pages/contact/Contact";
+const Contact = lazy(() => import("./pages/contact/Contact"));
+
+// import About from "./pages/about/About";
+const About = lazy(() => import("./pages/about/About"));
 
 function App() {
   const location = useLocation();
